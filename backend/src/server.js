6053829d72cd94942e973a707f391937a320ef00
zombie5454 <b08902129@ntu.edu.tw>
2024-07-29 00:05:01 +0800

    set Access-Control-Allow-Origin to all response header

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,18 @@ import newsAPI from './newsAPI.js';
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // 处理 OPTIONS 请求（预检请求）
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+  
+    next();
+  });
 app.use(express.json());
 app.use('/', routes);
 
