8961cf82e83d373f4639ec28925de976862861ac
zombie5454 <b08902129@ntu.edu.tw>
2024-07-29 09:11:31 +0800

    setHeader to solve COR issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,18 +7,28 @@ import newsAPI from './newsAPI.js';
 
 const app = express();
 app.use(cors());
+
+// Set middleware of CORS 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-    // 处理 OPTIONS 请求（预检请求）
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-  
-    next();
-  });
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://redanews.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 app.use(express.json());
 app.use('/', routes);
 
