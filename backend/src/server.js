65a7df585af7c3937bfd901457ed0fe5919a9962
zombie5454 <b08902129@ntu.edu.tw>
2024-07-29 09:37:02 +0800

    try to solve cor issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://redanews.onrender.com"
+    "*"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
