1e111826322bcaaa82beab481456402e46044b57
zombie5454 <b08902129@ntu.edu.tw>
2024-07-29 10:08:22 +0800

    try to slove cors issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,26 +6,27 @@ import newsAPI from './newsAPI.js';
 
 
 const app = express();
-app.use(cors());
-
+//app.use(cors());
+/*app.use(cors({
+  origin: '*', // 或者設置具體的來源
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
+  credentials: true,
+  maxAge: 7200
+}));*/
 // Set middleware of CORS 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "*"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
   res.setHeader("Access-Control-Max-Age", 7200);
+
+  // 處理預檢請求
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
 
   next();
 });
